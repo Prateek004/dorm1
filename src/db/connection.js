c0b5ede21a5cff2cdd43b1a9be87b363ef2097d6
Bill bharat.co.in <117995755+Prateek004@users.noreply.github.com>
2026-09-15 00:29:16 +0530

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,9 +2,7 @@
 
 let _db = null;
 
-function setDb(db) {
-  _db = db;
-}
+function setDb(db) { _db = db; }
 
 function getDb() {
   if (!_db) throw new Error('Database not initialized. Call setDb(db) from server.js first.');
